61723571655cdca713f760f8c8edb229ba4eda15
Jerous <tlu37317@gmail.com>
2017-06-13 15:19:05 +0800

    add controller and view msg

--- site/helloworld.php
+++ site/helloworld.php
@@ -1,1 +1,24 @@
-Hello World
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// Get an instance of the controller prefixed by HelloWorld = HelloWorldController
+$controller = JControllerLegacy::getInstance('HelloWorld');
+ 
+// Perform the Request task
+// URL:// index.php?option=com_helloworld&task=<task_name>
+// If no task is set, the default task 'display' will be assumed.
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+ 
+// Redirect if set by the controller
+$controller->redirect();
+?>
