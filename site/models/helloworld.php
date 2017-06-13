f0115dab456e2f07dc92b36e4c99b03123626836
Jerous <tlu37317@gmail.com>
2017-06-13 16:32:23 +0800

    Using the database refactor variable request menu type

--- site/models/helloworld.php
+++ site/models/helloworld.php
@@ -21,36 +21,68 @@ class HelloWorldModelHelloWorld extends JModelItem
 	 * @var string message
 	 */
 	protected $message;
- 
+  
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
+	 */
+	public function getTable($type = 'HelloWorld', $prefix = 'HelloWorldTable', $config = array())
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
+
 	/**
 	 * Get the message
-         *
-	 * @return  string  The message to be displayed to the user
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
 	 */
-	public function getMsg()
+	public function getMsg($id = 1)
 	{
-		if (!isset($this->message))
+		if (!is_array($this->messages))
+		{
+			$this->messages = array();
+		}
+
+		if (!isset($this->message[$id]))
 		{
 			// $this->message = 'Hello World!';
 
       $jinput = JFactory::getApplication()->input;
 			$id     = $jinput->get('id', 1, 'INT');
  
-			switch ($id)
-			{
-        // index.php?option=com_helloworld&view=helloworld&id=2
-				case 2:
-					$this->message = 'Good bye World!';
-					break;
-				default:
-        // index.php?option=com_helloworld&view=helloworld&id=1
-				case 1:
-					$this->message = 'Hello World!';
-					break;
-			}
+			// switch ($id)
+			// {
+      //   // index.php?option=com_helloworld&view=helloworld&id=2
+			// 	case 2:
+			// 		$this->message = 'Good bye World!';
+			// 		break;
+			// 	default:
+      //   // index.php?option=com_helloworld&view=helloworld&id=1
+			// 	case 1:
+			// 		$this->message = 'Hello World!';
+			// 		break;
+			// }
+
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+ 
+			// Load the message
+			$table->load($id);
+ 
+			// Assign the message
+			$this->messages[$id] = $table->greeting;
 		}
  
-		return $this->message;
+		return $this->message[$id];
 	}
 }
 ?>
